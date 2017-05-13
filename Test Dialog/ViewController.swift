fdbf07b279a4b207860400f8ff2980f1cedd8fd8
Siwawes Wongcharoen <siwaoh@gmail.com>
2017-05-13 13:43:00 +0700

    Initial Commit

---
+++ Test Dialog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test Dialog
+//
+//  Created by Siwawes Wongcharoen on 5/13/2560 BE.
+//  Copyright © 2560 siwaweswongcharoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
